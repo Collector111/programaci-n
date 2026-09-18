b20a9fbb15d8b8722e33fb7374c685e004a0bee4
unknown <aitormt03@gmail.com>
2026-09-18 20:35:02 +0100

    actividad 2 añadida(condicionales)

---
+++ Practicar/act2/act2.java
@@ -0,0 +1,21 @@
+package pro.Practicar.act2;
+
+import java.util.Scanner;
+
+public class act2 {
+    public static void main(String[] args) {
+        Scanner leer= new Scanner(System.in);
+
+        int num;
+
+        System.out.println("Introduce un numero: ");
+        num=leer.nextInt();
+
+        if(num%2==0){
+            System.out.println("Este numero es par");
+        }else{
+            System.out.println("Este numero es impar");
+        }
+        leer.close();
+    }
+}
