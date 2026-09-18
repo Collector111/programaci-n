8c1de5ba20ec23205306d2d9a138f7a499a76a8f
unknown <aitormt03@gmail.com>
2026-09-18 15:44:52 +0100

    Tarea de prueba hecha(pendiente de corregir)

---
+++ Salidastring/Salidastring.java
@@ -0,0 +1,11 @@
+package pro.Salidastring;
+public class Salidastring{
+    public static void main(String[] args){
+        int resultado;
+        resultado=2*5;
+        System.out.print("2 x 5= ");
+        System.out.print(resultado);
+        System.out.println();
+        System.out.print(3+5);
+    }
+}
