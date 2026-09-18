38240937ef4cd84a09443c46473e4eb46aa4fcd9
unknown <aitormt03@gmail.com>
2026-09-18 20:19:26 +0100

    Aportacion de una carpeta para practicas personales

---
+++ Practicar/act1/act1.java
@@ -0,0 +1,22 @@
+package pro.Practicar.act1;
+
+import java.util.Scanner;
+
+public class act1 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int num1;
+        int num2;
+
+        System.out.println("Introduce un numero:");
+        num1=leer.nextInt();
+        System.out.println("Introduce otro numero:");
+        num2=leer.nextInt();
+
+        int suma=num1+num2;
+
+        System.out.println("El resultado es: " + suma);
+
+        leer.close();
+    }
+}
