45401db2df13de49baf31be14d132de796228faa
Collector111 <aitormt03@gmail.com>
2026-09-19 23:03:57 +0100

    creacion de un array en act4

---
+++ Practicar/act4/act4.java
@@ -0,0 +1,12 @@
+package Practicar.act4;
+
+public class act4 {
+    //Crear un array con 5 valores e imprime su longitud
+    public static void main(String[] args) {
+        int[] valores= new int[5];
+        
+        System.out.println(valores.length);
+    }
+   
+    
+}
