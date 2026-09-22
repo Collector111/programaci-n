6447ff49f5678bee49970da02c5dcf0e1f9d86e4
Collector111 <aitormt03@gmail.com>
2026-09-22 23:46:40 +0100

    act5(menu) realizada esta actividad implementando random,while,entrada de teclado y switch

---
+++ Practicar/bucles/act5w/act5w.java
@@ -0,0 +1,49 @@
+package Practicar.bucles.act5w;
+import java.util.Scanner;
+import java.util.Random;
+
+public class act5w {
+    /*Menú interactivo
+Crea un menú que se repita hasta que el usuario seleccione "Salir":
+
+1. Saludar
+2. Mostrar un número
+3. Mostrar los números del 1 al 10
+4. Salir */
+public static void main(String[] args) {
+    Scanner leer=new Scanner(System.in);
+    Random random=new Random();
+    int order;
+    int banderin=0;
+    
+
+    while (banderin==0) {
+        int randomcounter=random.nextInt(100);
+        System.out.println("Menu:");
+        System.out.println("1. Saludar");
+        System.out.println("2. Mostrar numero random");
+        System.out.println("3. Mostrar numeros del 1-10");
+        System.out.println("4. Salir");
+        System.out.println("<-------------------------------->");
+        order=leer.nextInt();
+
+        switch (order) {
+            case 1:
+                System.out.println("Hola Usuario totalmente desconocido");
+                break;
+            case 2:
+                System.out.println(randomcounter);
+                break;
+            case 3:
+                System.out.println("1,2,3,4,5,6,7,8,9,10");
+                break;
+            default:
+                banderin=1;
+                break;
+        }
+
+    }
+    leer.close();
+}
+
+}
