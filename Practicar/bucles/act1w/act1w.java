fff1e9a4c024ce1e5380af0844ef1a22ede295bd
unknown <aitormt03@gmail.com>
2026-09-22 08:11:51 +0100

    nueva carpeta de bucles para repaso act1 de bubles(repaso) realizada

---
+++ Practicar/bucles/act1w/act1w.java
@@ -0,0 +1,15 @@
+package Practicar.bucles.act1w;
+
+public class act1w {
+
+    public static void main(String[] args) {
+        int num=2;
+
+        while (num<=20) {
+            if(num%2==0){
+                System.out.println(num);
+            }
+            num++;
+        }
+    }
+}
